48b0ccfe6c3dfc06a754fbe3e5e6039a5561f1b9
Fayrouz20 <aitabdelaalifayrouz01@gmial.com>
2023-03-21 09:04:23 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *return: always 0
+*/
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb <= 9; nb++)
+	{
+		putchar(nb + 48);
+		putchar(",");
+		putchar(" ");
+	}
+	putchar('\n');
+	return (0);
+}
