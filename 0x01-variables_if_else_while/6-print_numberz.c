4c8b9424b901d084675420aefeb04c7a0db2a743
Fayrouz20 <aitabdelaalifayrouz01@gmial.com>
2023-03-21 08:40:01 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb <= 9; nb++)
+	{
+		putchar(nb + 48);
+	}
+	putchar('\n');
+	return (0);
+}
+
