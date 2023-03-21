5596f22273698d2e1ae9c9867804292637396bc0
Fayrouz20 <aitabdelaalifayrouz01@gmial.com>
2023-03-21 08:31:18 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	int nb;
+
+	for (nb = 0; nb <= 9; nb++)
+	{
+		printf("%d",nb);
+	}
+	putchar('\n');
+	return (0);
+}
