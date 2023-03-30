3eb8ea377f7fff4d979d71115268d3070de81072
Fayrouz20 <aitabdelaalifayrouz01@gmial.com>
2023-03-30 05:18:22 -0700

    0. strcat

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *puts_half - function
+ *@str : variable
+ *Return: void
+*/
+void puts_half(char *str)
+{
+	int length;
+	int n;
+	int i;
+
+	length = 0;
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	n = (length - 1) / 2;
+	for (i = n ; i > 0 ; i--)
+	{
+		putchar(*str);
+	}
+	putchar('\n');
+}
