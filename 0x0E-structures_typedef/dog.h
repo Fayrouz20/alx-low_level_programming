ef9b48d9666beaddb3a3d94663ca5ba7116e6401
Fayrouz20 <aitabdelaalifayrouz01@gmial.com>
2023-04-17 09:12:25 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - structure
+ * @name : name of the dogs
+ * @age : age of the dogs
+ * @owner : the owner of the dog
+ * Description: structure of dogs
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
